3c0077de4a752166fb8c634490ed9bb6ded299d3
Hoangnguyenduc10-06-2006kiu <gochoctap102@gmail.com>
2024-12-11 13:21:54 +0700

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void creatAndPrint(int row,int col){
+	int i=0,j,arr[row][col];
+	while(i<row){
+		j=0;
+		while(j<col){
+			printf("vui long nhap gia tri vi tri [%d][%d]",i,j);
+			scanf("%d",&arr[i][j]);
+			j++;
+		}
+		i++;
+	}
+	i=0;
+	while(i<row){
+		j=0;
+		while(j<col){
+			printf("%d ",arr[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+}
+int main(){
+	int n,m;
+	printf("vui long nhap so hang: ");
+	scanf("%d",&n);
+	printf("vui long nhap so cot: ");
+	scanf("%d",&m);
+	creatAndPrint(n,m);
+	return 0;
+}
+
